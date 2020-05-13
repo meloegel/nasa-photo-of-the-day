8ab70c4ab8efda4092baf6c7a5a0cc3816cf412e
meloegel <meloegel@gmail.com>
2020-05-13 18:35:59 -0400

    mark-loegel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './constants';
 
 function App() {
+const [potd, setPotd] = useState(1)
+
+
+useEffect(() => {
+  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+  .then(res => {
+    setPotd(res.data)
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}, [])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{potd.title}</h1>
+      <img src={potd.url} alt={potd.title}/>
+      <p>{potd.date}</p>
+      <p>{potd.explanation} </p>
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = '7oc2mjOif2e0NT04dIEtqXAyAHCJsVb5zAVFNjGs'
