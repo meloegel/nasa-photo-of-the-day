ed66576fc52eaf939e7901e9d8c8afc3f9a2bc8c
meloegel <meloegel@gmail.com>
2020-10-27 19:40:15 -0400

    fixing alot of sloppy code

--- src/App.js
+++ src/App.js
@@ -3,61 +3,61 @@ import "./App.css";
 import axios from 'axios';
 import { BASE_URL, API_KEY } from './constants';
 import nasaLogo from './nasa-logo.png';
+import ReactPlayer from "react-player"
 
-import StyledHOne from './h1'
-import StyledDiv from './div'
-import StyledSection from './container'
-import StyledP from './p'
-import StyledImg from './img'
-import StyledHTwo from './h2'
 import StyledPinput from './pInput'
-import { withTheme } from "styled-components";
+
 
 
 function App() {
-const moment = require('moment')
-const momentDate = moment().format('YYYY-MM-DD')
+  const moment = require('moment')
+  const momentDate = moment().format('YYYY-MM-DD')
 
-const [potd, setPotd] = useState(1)
-const [date, setDate] = useState('')
+  const [potd, setPotd] = useState(1)
+  const [date, setDate] = useState('')
 
-const inputNewDate = event => {
-  setDate(event.target.value)
-  
-}
+  const inputNewDate = event => {
+    setDate(event.target.value)
 
+  }
 
-useEffect(() => {
-  axios.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
-  .then(res => {
-    setPotd(res.data)
-    setDate(res.data.date)
-    console.log(res.data.date)
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}, [date])
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
+      .then(res => {
+        setPotd(res.data)
+        setDate(res.data.date)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [date])
 
 
   return (
 
-    <StyledSection className="App">
-      <img src={nasaLogo} alt='nasa-logo'/>
-      <StyledHOne>{potd.title}</StyledHOne>
-      <StyledHTwo>{potd.date}</StyledHTwo>
-      <StyledPinput>Date:<input type='date' value={date} onChange={inputNewDate} style={inputStyle}/></StyledPinput>
-      <StyledDiv>
-        <StyledImg src={potd.url} alt={potd.title} />
-        <StyledP>{potd.explanation} </StyledP>
-      </StyledDiv>
-    </StyledSection>
+    <div className="App">
+      <img src={nasaLogo} alt='nasa-logo' id='nasaLogo' />
+      <div className='topContent'>
+        <h1>{potd.title}</h1>
+        <StyledPinput>Date:<input type='date' value={date} onChange={inputNewDate} style={inputStyle} /></StyledPinput>
+        <h2>{potd.date}</h2>
+      </div>
+      <div className='contentDiv'>
+        {potd.media_type === 'video' ? (
+          <ReactPlayer width="45%" height="auto" controls='autoPlay' url={potd.url} alt={potd.title} />
+        ) : (
+            <img id='images' src={potd.url} alt={potd.title} />
+          )}
+        <p className='about'>{potd.explanation} </p>
+      </div>
+    </div>
   );
 }
 
 export default App;
 
 const inputStyle = {
-  background:'#0b3d91',
+  background: '#0b3d91',
   color: 'white'
 }
--- src/pInput.jsx
+++ src/pInput.jsx
@@ -1,25 +1,22 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const StyledPinput = styled.p`
-color: white;
-text-align:center;
-text-shadow: 2px 2px 4px gray;
-font-size:1.2rem; 
-background-color: #0b3d91;
-border:1px solid #fc3d21;
-box-shadow: 8px 12px #fc3d21;
-text-align:center;
-text-shadow: 4px 4px 8px #fc3d21;
-width:20%;
-margin-left: auto;
-margin-right: auto;
-margin-bottom: 2rem;
-border-radius: 2.5rem;
-input{
+  color: white;
+  text-align: center;
+  text-shadow: 2px 2px 4px gray;
+  font-size: 1.2rem;
+  background-color: #0b3d91;
+  border: 1px solid #fc3d21;
+  box-shadow: 8px 12px #fc3d21;
+  text-align: center;
+  text-shadow: 4px 4px 8px #fc3d21;
+  width: 20%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2rem;
+  border-radius: 2.5rem;
+  input {
+  }
+`;
 
-}
-
-`
-
-
-export default StyledPinput
+export default StyledPinput;
