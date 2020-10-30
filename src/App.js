399d633bde43f768a710064f1ca979db8c2a487b
meloegel <meloegel@gmail.com>
2020-10-30 18:11:04 -0400

    added created by

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ function App() {
             <div className="aboutTitle"> About Nasa Photo of The Day </div>
             <div className="aboutPopupDiv">
               <p className='aboutPopupContent'>Nasa Photo of the day was created unsing the Nasa public daily photo api. Each day Nasa will have a different photo or video to display. On this page you can navigate thought photos of the past utilizing the date selection. You can also view the photos in HD by clicking on them, click again to close.</p>
+              <p id='createdBy'>Created By: <a target="_blank" href='https://github.com/meloegel/nasa-photo-of-the-day'>Mark Loegel</a></p>
             </div>
             <div className="buttonPopup">
               <button className="buttonClose" onClick={() => { close(); }}>Close</button>
