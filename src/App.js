80ec75d5c771f161c8496bc93c4d9a91ca0874a7
meloegel <meloegel@gmail.com>
2021-02-18 20:43:09 -0500

    deployment mapbox fix attempt 1

--- src/App.js
+++ src/App.js
@@ -86,10 +86,10 @@ function App() {
           )}
         </Popup>
       </div>
-      {/* <div>
+      <div>
         <h3 className='issTitle'>Live Tracking The International Space Station</h3>
         <IssMap />
-      </div> */}
+      </div>
     </div>
   );
 }
